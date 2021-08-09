68de505ca15f8038ef9d41c499657a1fa14697cc
Stefan Gouldson <stefan@Stefans-MacBook-Pro-Cool-Studio.local>
2021-08-09 17:01:49 +0100

    ssg

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,32 @@
 import MeetupList from '../components/meetups/MeetupList'
 
-const HomePage = () => {
+const MEETUPS = [
+  {
+    id: 'm1',
+    title: 'A First Meetup',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Palace_of_Westminster_from_the_dome_on_Methodist_Central_Hall_%28cropped%29.jpg/1000px-Palace_of_Westminster_from_the_dome_on_Methodist_Central_Hall_%28cropped%29.jpg',
+    address: 'London'
+  },
+  {
+    id: 'm2',
+    title: 'A Second Meetup',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Palace_of_Westminster_from_the_dome_on_Methodist_Central_Hall_%28cropped%29.jpg/1000px-Palace_of_Westminster_from_the_dome_on_Methodist_Central_Hall_%28cropped%29.jpg',
+    address: 'London'
+  },
+]
 
-  const MEETUPS = [
-    {
-      id: 'm1',
-      title: 'A First Meetup',
-      image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Palace_of_Westminster_from_the_dome_on_Methodist_Central_Hall_%28cropped%29.jpg/1000px-Palace_of_Westminster_from_the_dome_on_Methodist_Central_Hall_%28cropped%29.jpg',
-      address: 'London'
-    }
-  ]
+const HomePage = (props) => {
   return (
-      <MeetupList meetups={MEETUPS} />
+      <MeetupList meetups={props.meetups} />
   )
+}
+
+export async function getStaticProps() {
+  return {
+    props: {
+      meetups: MEETUPS
+    }
+  }
 }
 
 export default HomePage
